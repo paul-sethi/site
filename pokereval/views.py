a002b0df2f17337775a9fb68ff76b0a47bff7a3d
Paul <sethi.paul@gmail.com>
2016-06-07 17:01:30 -0400

    Add basic poker eval

--- pokereval/views.py
+++ pokereval/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import pbots_calc
+
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Holla back.")
+	r = pbots_calc.calc("ATs:89s","","",1000000)
+	if r:
+		return HttpResponse(str(r))
+	else:
+		return HttpResponse("error")
