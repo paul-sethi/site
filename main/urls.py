9b736e346545fb637766538794ee7668746510a5
Paul <sethi.paul@gmail.com>
2016-06-08 08:40:55 -0400

    Make base template

--- main/urls.py
+++ main/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', TemplateView.as_view(template_name="index2.html")),
     url(r'^admin/', admin.site.urls),
 	url(r'^pokereval/', include('pokereval.urls')),
 ]
